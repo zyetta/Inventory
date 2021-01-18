e3151ed4b2f247631f41b15b526cf78a06626210
zyetta <jeanmarcmaree@protonmail.com>
2021-01-19 01:32:43 +0200

    first commit

--- DatabaseMain.h
+++
@@ -1,7 +0,0 @@
-#ifndef DATABASEMAIN_H_INCLUDED
-#define DATABASEMAIN_H_INCLUDED
-
-void CreateDatabase(sqlite3 *db, const char* filename);
-static int callback(void *NotUsed, int argc, char **argv, char **szColName);
-
-#endif // DATABASEMAIN_H_INCLUDED
